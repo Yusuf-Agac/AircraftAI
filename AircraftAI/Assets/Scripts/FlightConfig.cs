5ffbe373770db30a468dcb602c5bbd9cc4d3ba9e
Yusuf AĞAÇ <yusuftree1@hotmail.com>
2024-05-11 19:22:06 +0300

    FEAT: Behavior Subclasses

---
+++ AircraftAI/Assets/Scripts/FlightConfig.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Oyedoyin.FixedWing;
+using Unity.MLAgents.Policies;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.Serialization;
+using UnityEngine.UI;
+
+[Serializable]
+class FlightConfig : BehaviorConfig
+{
+    [Space(10)]
+    [SerializeField] private FlightPathNormalizer flightPathNormalizer;
+    [SerializeField] private FixedController aircraftController;
+    [SerializeField] private Slider pitchSlider;
+    [SerializeField] private Slider rollSlider;
+    [SerializeField] private Slider throttleSlider;
+    
+    public override void SetBehaviorComponent(Transform transform)
+    {
+        AddBehaviorComponent(transform);
+        
+        var aircraftFlightAgent = transform.AddComponent<AircraftFlightAgent>();
+        Agent = aircraftFlightAgent;
+        
+        aircraftFlightAgent.MaxStep = maxStep;
+        
+        aircraftFlightAgent.trainingMode = false;
+        aircraftFlightAgent.manoeuvreSpeed = manoeuvreSpeed;
+        aircraftFlightAgent.maxWindSpeed = maxWindSpeed;
+        aircraftFlightAgent.maxTurbulence = maxTurbulence;
+        aircraftFlightAgent.numOfOptimumDirections = numOfOptimumDirections;
+        aircraftFlightAgent.gapBetweenOptimumDirections = gapBetweenOptimumDirections;
+        
+        aircraftFlightAgent.flightPathNormalizer = flightPathNormalizer;
+        flightPathNormalizer.aircraftAgents.Clear();
+        flightPathNormalizer.aircraftAgents.Add(aircraftFlightAgent);
+        
+        aircraftFlightAgent.aircraftController = aircraftController;
+        aircraftFlightAgent.pitchSlider = pitchSlider;
+        aircraftFlightAgent.rollSlider = rollSlider;
+        aircraftFlightAgent.throttleSlider = throttleSlider;
+        
+        AddDecisionRequester(transform);
+    }
+}
---
+++ AircraftAI/Assets/Scripts/TakeOffConfig.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Oyedoyin.FixedWing;
+using Unity.MLAgents.Policies;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.Serialization;
+using UnityEngine.UI;
+
+[Serializable]
+class TakeOffConfig : BehaviorConfig
+{
+    [Space(10)]
+    [SerializeField] private AirportNormalizer airportNormalizer;
+    [SerializeField] private AircraftCollisionSensors sensors;
+    [SerializeField] private AircraftRelativeTransformCanvas relativeTransformCanvas;
+    [SerializeField] private FixedController aircraftController;
+    [SerializeField] private Slider pitchSlider;
+    [SerializeField] private Slider rollSlider;
+    [SerializeField] private Slider throttleSlider;
+    
+    public override void SetBehaviorComponent(Transform transform)
+    {
+        AddBehaviorComponent(transform);
+        
+        AircraftTakeOffAgent aircraftTakeOffAgent = transform.AddComponent<AircraftTakeOffAgent>();
+        Agent = aircraftTakeOffAgent;
+        
+        aircraftTakeOffAgent.MaxStep = maxStep;
+
+        aircraftTakeOffAgent.trainingMode = false;
+        aircraftTakeOffAgent.manoeuvreSpeed = manoeuvreSpeed;
+        aircraftTakeOffAgent.maxWindSpeed = maxWindSpeed;
+        aircraftTakeOffAgent.maxTurbulence = maxTurbulence;
+        aircraftTakeOffAgent.numOfOptimumDirections = numOfOptimumDirections;
+        aircraftTakeOffAgent.gapBetweenOptimumDirections = gapBetweenOptimumDirections;
+        
+        aircraftTakeOffAgent.airportNormalizer = airportNormalizer;
+        airportNormalizer.aircraftAgents.Clear();
+        airportNormalizer.aircraftAgents.Add(aircraftTakeOffAgent);
+        
+        aircraftTakeOffAgent.sensors = sensors;
+        aircraftTakeOffAgent.relativeTransformCanvas = relativeTransformCanvas;
+        aircraftTakeOffAgent.aircraftController = aircraftController;
+        aircraftTakeOffAgent.PitchSlider = pitchSlider;
+        aircraftTakeOffAgent.RollSlider = rollSlider;
+        aircraftTakeOffAgent.ThrottleSlider = throttleSlider;
+        
+        AddDecisionRequester(transform);
+    }
+}
