088337a6500f97699f520af587591f1756886ce9
Yusuf AĞAÇ <yusuftree1@hotmail.com>
2024-05-14 15:13:29 +0300

    FIX: Fıxed Wind Change Speed

--- AircraftAI/Assets/Scripts/AtmosphereController.cs
+++ AircraftAI/Assets/Scripts/AtmosphereController.cs
@@ -5,20 +5,20 @@ namespace DefaultNamespace
 {
     public static class AtmosphereController
     {
-        public static void SmoothlyChangeWindAndTurbulence(FixedController fixedController, float maxWindSpeed, float maxTurbulence, int decisionPeriod)
+        public static void SmoothlyChangeWindAndTurbulence(FixedController fixedController, float maxWindSpeed, float maxTurbulence, int decisionPeriod, float windDirectionSpeed)
         {
             var windDir = (float)fixedController.m_core.m_atmosphere.m_ψw;
             var windSpeed = (float)fixedController.m_core.m_atmosphere.m_windSpeed;
             var turbulence = (float)fixedController.m_core.m_atmosphere.m_turbulence;
             
-            windDir += Random.Range(-150f, 150f) * (decisionPeriod / 25f);
+            windDir += Random.Range(-1f, 1f) * (decisionPeriod / 25f) * windDirectionSpeed;
             while(windDir < 0) windDir += 360;
             fixedController.m_core.m_atmosphere.m_ψw = (windDir % 360);
             
-            windSpeed += Random.Range(-1f * maxWindSpeed, 1f * maxWindSpeed) * (decisionPeriod / 25f);
+            windSpeed += Random.Range(-2f * maxWindSpeed, 2f * maxWindSpeed) * (decisionPeriod / 25f);
             fixedController.m_core.m_atmosphere.m_windSpeed = Mathf.Clamp(windSpeed, 0, maxWindSpeed);
             
-            turbulence += Random.Range(-1f * maxTurbulence, 1f * maxTurbulence) * (decisionPeriod / 25f);
+            turbulence += Random.Range(-2f * maxTurbulence, 2f * maxTurbulence) * (decisionPeriod / 25f);
             fixedController.m_core.m_atmosphere.m_turbulence = Mathf.Clamp(turbulence, 0, maxTurbulence);
         }
     }
