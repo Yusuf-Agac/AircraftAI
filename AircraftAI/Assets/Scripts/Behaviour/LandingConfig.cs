3e80cb8307462264e133c6b422b982924cc5efa4
Yusuf AĞAÇ <yusuftree1@hotmail.com>
2024-05-29 00:45:39 +0300

    FEAT: Update Combined Flight With Landing Phase

---
+++ AircraftAI/Assets/Scripts/Behaviour/LandingConfig.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Oyedoyin.FixedWing;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+[Serializable]
+class LandingConfig : BehaviorConfig
+{
+    [Space(10)]
+    [SerializeField] private AirportNormalizer airportNormalizer;
+    [SerializeField] private AircraftCollisionSensors sensors;
+    
+    public override void SetBehaviorComponent(Transform transform)
+    {
+        AddBehaviorComponent(transform);
+        
+        var aircraftTakeOffAgent = transform.AddComponent<AircraftLandingAgent>();
+        Agent = aircraftTakeOffAgent;
+        
+        aircraftTakeOffAgent.MaxStep = maxStep;
+
+        aircraftTakeOffAgent.trainingMode = false;
+        aircraftTakeOffAgent.manoeuvreSpeed = manoeuvreSpeed;
+        aircraftTakeOffAgent.windDirectionSpeed = windDirectionSpeed;
+        aircraftTakeOffAgent.maxWindSpeed = maxWindSpeed;
+        aircraftTakeOffAgent.maxTurbulence = maxTurbulence;
+        aircraftTakeOffAgent.numOfOptimalDirections = numOfOptimumDirections;
+        aircraftTakeOffAgent.gapBetweenOptimalDirections = gapBetweenOptimumDirections;
+        
+        aircraftTakeOffAgent.airportNormalizer = airportNormalizer;
+        airportNormalizer.aircraftLandingAgents.Clear();
+        airportNormalizer.aircraftLandingAgents.Add(aircraftTakeOffAgent);
+        
+        aircraftTakeOffAgent.sensors = sensors;
+        aircraftTakeOffAgent.observationCanvas = observationCanvas;
+        aircraftTakeOffAgent.rewardCanvas = rewardCanvas;
+        aircraftTakeOffAgent.aircraftController = aircraftController;
+        
+        AddDecisionRequester(transform);
+    }
+}
