1c74595ee078ade589dd2cd6835f18494fe1d394
Yusuf AĞAÇ <yusuftreeg@gmail.com>
2025-05-01 15:12:04 +0300

    FEAT: flight agent property drawer

---
+++ AircraftAI/Assets/Scripts/Agent/Config/Editor/FlightAgentEditor.cs
@@ -0,0 +1,18 @@
+﻿using UnityEditor;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+[CustomEditor(typeof(AircraftFlightAgent))]
+public class FlightAgentEditor : Editor
+{
+    [SerializeField] private VisualTreeAsset visualTreeAsset;
+    
+    public override VisualElement CreateInspectorGUI()
+    {
+        VisualElement visualElement = new();
+        
+        visualTreeAsset.CloneTree(visualElement);
+        
+        return visualElement;
+    }
+}
