cea4596a530ec3cd854d700d806ea56aa25e01bb
Yusuf AĞAÇ <yusuftreeg@gmail.com>
2025-05-01 15:11:25 +0300

    FEAT: take off agent property drawer

---
+++ AircraftAI/Assets/Scripts/Agent/Config/Editor/TakeOffAgentEditor.cs
@@ -0,0 +1,18 @@
+﻿using UnityEditor;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+[CustomEditor(typeof(AircraftTakeOffAgent))]
+public class TakeOffAgentEditor : Editor
+{
+    [SerializeField] private VisualTreeAsset visualTreeAsset;
+    
+    public override VisualElement CreateInspectorGUI()
+    {
+        VisualElement visualElement = new();
+        
+        visualTreeAsset.CloneTree(visualElement);
+        
+        return visualElement;
+    }
+}
